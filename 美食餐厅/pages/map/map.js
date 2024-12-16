3490329c1847ef682c6e6cee47ea88b745a2eece
CurlyFei <tangchao321088@gmail.com>
2024-12-16 10:31:35 +0800

    main

--- 美食餐厅/pages/map/map.js
+++ 美食餐厅/pages/map/map.js
@@ -1,4 +1,4 @@
-const key = '2PYBZ-RUKOA-RRMKA-CW7LK-LSBNF-XUFQI'; 
+const key = 'OQCBZ-I363A-M34KB-CJS4E-RY2N6-JVBC6'; 
 const QQMapWX = require('../../libs/qqmap-wx-jssdk.js');
 const qqmapsdk = new QQMapWX({ key });
 
@@ -8,7 +8,11 @@ Page({
     longitude: 0,
     latitude: 0,
     markers: [],
-    mapCtx: null
+    mapCtx: null,
+    searchStats: {
+      total: 0,
+      searching: false
+    }
   },
 
   onLoad: function () {
@@ -20,44 +24,98 @@ Page({
           latitude: res.latitude
         });
         this.getFood(res.longitude, res.latitude);
+      },
+      fail: err => {
+        wx.showToast({
+          title: '获取位置失败',
+          icon: 'none'
+        });
       }
     });
   },
 
   onReady: function () {
-    this.mapCtx = wx.createMapContext('myMap');
+    this.setData({
+      mapCtx: wx.createMapContext('myMap', this)
+    });
   },
 
   getFood: function (longitude, latitude) {
+    this.setData({
+      'searchStats.searching': true,
+      markers: []
+    });
+
     qqmapsdk.search({
       keyword: '餐厅',
       location: { longitude, latitude },
+      radius: 2000,
+      page_size: 20,
       success: res => {
-        const markers = res.data.map((item, index) => ({
+        if (!res.data || res.data.length === 0) {
+          this.setData({
+            'searchStats.searching': false,
+            'searchStats.total': 0,
+            markers: []
+          });
+          wx.showToast({
+            title: '附近没有找到餐厅',
+            icon: 'none'
+          });
+          return;
+        }
+
+        const newMarkers = res.data.map((item, index) => ({
           iconPath: '/images/food.png',
           id: index,
           latitude: item.location.lat,
           longitude: item.location.lng,
           width: 30,
           height: 30,
-          title: item.title
+          title: item.title,
+          address: item.address,
+          distance: item._distance,
+          callout: {
+            content: item.title,
+            padding: 10,
+            borderRadius: 5,
+            display: 'BYCLICK'
+          }
         }));
-        markers.push({
+        
+        newMarkers.push({
           iconPath: '/images/center.png',
-          id: res.data.length,
+          id: -1,
           latitude,
           longitude,
           width: 15,
           height: 40
         });
-        this.setData({ markers });
+
+        this.setData({
+          markers: newMarkers,
+          'searchStats.total': newMarkers.length - 1,
+          'searchStats.searching': false
+        });
+      },
+      fail: err => {
+        console.error('搜索失败:', err);
+        this.setData({
+          'searchStats.searching': false,
+          'searchStats.total': 0,
+          markers: []
+        });
+        wx.showToast({
+          title: '搜索餐厅失败',
+          icon: 'none'
+        });
       }
     });
   },
 
   regionChange: function (e) {
-    if (e.type === 'end') {
-      this.mapCtx.getCenterLocation({
+    if (e.type === 'end' && this.data.mapCtx) {
+      this.data.mapCtx.getCenterLocation({
         success: res => {
           this.getFood(res.longitude, res.latitude);
         }
@@ -66,12 +124,40 @@ Page({
   },
 
   controlTap: function () {
-    this.mapCtx.moveToLocation();
+    if (this.data.mapCtx) {
+      this.data.mapCtx.moveToLocation();
+    }
   },
 
   bannerTap: function () {
     wx.navigateTo({
       url: '/pages/coupon/coupon'
     });
+  },
+
+  markertap: function(e) {
+    const marker = this.data.markers.find(m => m.id === e.markerId);
+    if (marker && marker.title && marker.id !== -1) {
+      wx.showActionSheet({
+        itemList: ['查看详情', '导航到这里'],
+        success: (res) => {
+          if (res.tapIndex === 0) {
+            wx.showModal({
+              title: marker.title,
+              content: `距离：${marker.distance}米\n地址：${marker.address}`,
+              showCancel: false
+            });
+          } else if (res.tapIndex === 1) {
+            wx.openLocation({
+              latitude: marker.latitude,
+              longitude: marker.longitude,
+              name: marker.title,
+              address: marker.address,
+              scale: 18
+            });
+          }
+        }
+      });
+    }
   }
-}) 
+}); 
